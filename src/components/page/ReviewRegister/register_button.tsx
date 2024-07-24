f3978404f4adb0aa0cbd172425f8a141642dc690
xxbeann <yjb4945@gmail.com>
2024-07-24 16:02:32 +0900

    feat: 강의평 등록개발_Header,star_rating,courese_review,reg_button

---
+++ src/components/page/ReviewRegister/register_button.tsx
@@ -0,0 +1,14 @@
+import { Rating, Button, Typography, Grid, TextField } from "@mui/material"
+import 'styles/review-register.css'
+
+const registerButton = () => {
+    return(
+        
+        <Button variant='contained' fullWidth className='submit-button'>
+        평가하기
+        </Button>
+
+    )
+}
+
+export default registerButton
