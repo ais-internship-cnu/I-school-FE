d9567e826d7ecbe7ba9fce77d1e9db927cb0dbcb
jiiiwonyy <ji__Won_22@naver.com>
2024-07-24 22:09:20 +0900

    feat: 강의평 등록하기 버튼 추가

--- src/components/page/Review/header.tsx
+++ src/components/page/Review/header.tsx
@@ -1,11 +1,22 @@
-import { Typography } from "@mui/material"
-import 'styles/review-register.css'
+import { Typography, Button, Grid } from "@mui/material";
+import 'styles/review-register.css';
 
 const Header = () => {
-    return(
-        <Typography variant="h6" className="rating-title">
-            강의평
-        </Typography>
-    )
+    return (
+        <Grid container className="header-container" alignItems="center">
+            <Grid item xs={4}></Grid>
+            <Grid item xs={4} container justifyContent="center">
+                <Typography variant="h6" className="rating-title">
+                    강의평
+                </Typography>
+            </Grid>
+            <Grid item xs={4} container justifyContent="flex-end">
+                <Button variant="contained" className="rating-button">
+                    평가하기
+                </Button>
+            </Grid>
+        </Grid>
+    );
 }
-export default Header
+
+export default Header;
