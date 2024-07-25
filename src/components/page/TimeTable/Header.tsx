6b745cb59f14a820cde2a08ade188ca435b1bcde
jiiiwonyy <ji__Won_22@naver.com>
2024-07-25 15:03:41 +0900

    feat: 타임테이블 헤더 추가

---
+++ src/components/page/TimeTable/Header.tsx
@@ -0,0 +1,35 @@
+import { Typography, Grid, IconButton } from "@mui/material"
+import ScheduleIcon from '@mui/icons-material/Schedule'
+import SettingsIcon from '@mui/icons-material/Settings'
+import AddIcon from '@mui/icons-material/Add'
+import 'styles/timetable.css'
+
+const Header = () => {
+    return (
+        <div>
+            <Grid container alignItems="center" justifyContent="space-between" className="header">
+                <Typography className="title-box">
+                    <Typography>
+                        2023학년 2학기
+                    </Typography>
+                    <Typography variant="h5" className="title">
+                        시간표
+                    </Typography>
+                </Typography>
+                <div>
+                    <IconButton>
+                        <AddIcon />
+                    </IconButton>
+                    <IconButton>
+                        <SettingsIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ScheduleIcon />
+                    </IconButton>
+                </div>
+            </Grid>
+        </div>
+    )
+}
+
+export default Header
