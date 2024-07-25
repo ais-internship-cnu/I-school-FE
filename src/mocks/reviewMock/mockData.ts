382c49b1f2c99188a0544eb70bd3e94c80c7aebd
Vomeee <ye43019@gmail.com>
2024-07-25 11:26:23 +0900

    feat: Mock data 추가

--- src/mocks/reviewMock/mockData.ts
+++ src/mocks/reviewMock/mockData.ts
@@ -5,5 +5,8 @@ export const mockCourses = [
     { courseName: 'Web Development', professor: 'Carol White', rating: 4.7 },
     { courseName: 'Machine Learning', professor: 'David Smith', rating: 4.9 },
     { courseName: 'Database Systems', professor: 'Emily Davis', rating: 4.5 },
+    { courseName: '데이터베이스시스템', professor: 'Emily Davis', rating: 4.5 },
+    { courseName: '운영체제', professor: 'Emily Davis', rating: 4.5 },
+    { courseName: '연애의 첫 단추', professor: 'Emily Davis', rating: 4.5 }
   ];
   
