817bf7a8e7ac3fd3e44651a25e696096c7340925
xxbeann <yjb4945@gmail.com>
2024-07-28 19:50:44 +0900

    feat: 예외처리 분기문 작성

--- src/redux/modules/review/index.ts
+++ src/redux/modules/review/index.ts
@@ -4,6 +4,7 @@ import { RootState } from 'redux/store'
 import { ReviewState } from 'types/review'
 import { ReviewRegisterTest } from 'types/reviewRegister'
 import { api } from 'utill/axios'
+import axios, { AxiosError } from 'axios';
 
 export const createReviewList = createAsyncThunk(
   'review/createReviewList',
@@ -26,8 +27,39 @@ export const createCourseReview = createAsyncThunk( // 정빈 추가 부분 API 
       console.log(response.status);
       alert('등록되었습니다.')
       // return response.data
-    } catch (error) {
-      alert('등록에 실패했습니다.')
+    } catch (err) { //예외처리
+      const error = err as AxiosError
+      if (error.response) {
+        // 서버가 응답을 반환한 경우
+        const { status } = error.response;
+        switch (status) {
+          case 400:
+            alert('잘못된 요청입니다. 입력한 내용을 다시 확인해주세요.');
+            break;
+          case 401:
+            alert('인증에 실패했습니다. 다시 로그인해주세요.');
+            break;
+          case 403:
+            alert('접근 권한이 없습니다.');
+            break;
+          case 404:
+            alert('리소스를 찾을 수 없습니다.');
+            break;
+          case 500:
+            alert('서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.');
+            break;
+          default:
+            alert('알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해주세요.');
+        }
+        return rejectWithValue(error.response.data);
+      } else if (error.request) {
+        // 요청이 전송되었으나 응답이 없는 경우
+        alert('서버로부터 응답이 없습니다. 네트워크 상태를 확인해주세요.');
+      } else {
+        // 요청 설정 중에 문제가 발생한 경우
+        alert('요청을 보내는 중에 오류가 발생했습니다.');
+      }
+      console.log(error.message);
       return rejectWithValue(error instanceof Error ? error.message : 'An error has occurred.');
     }
   }
