9160994ee4ba5073db283974e64e8704988fde76
jiiiwonyy <ji__Won_22@naver.com>
2024-07-30 13:29:54 +0900

    refactor: 변수명 표기법 적용

--- src/app/home-page/page.tsx
+++ src/app/home-page/page.tsx
@@ -1,10 +1,9 @@
 "use client"
 
 import Page from "app/page"
-import HomeButton from "components/page/Home/home-button"
-import NavBar from "components/page/Home/navigator"
-import HomeTitle from "components/page/Home/title"
-
+import HomeTitle from "components/page/Home/HomeTitle"
+import HomeButton from "components/page/Home/HomeButton"
+import NavBar from "components/page/Home/NavBar"
 
 const HomePage = () => {
   return(
