d6f49f7f60dba949eeb222900bb01bce528ff96c
jiiiwonyy <ji__Won_22@naver.com>
2024-07-30 13:11:06 +0900

    fix: 강의평 페이지에서 스크롤이 생기는 현상 수정

--- src/components/page/Review/navigation.tsx
+++ src/components/page/Review/navigation.tsx
@@ -1,40 +1,66 @@
-import { BottomNavigation, BottomNavigationAction, styled } from "@mui/material";
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 import React from "react";
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import PlagiarismOutlinedIcon from '@mui/icons-material/PlagiarismOutlined';
+import { styled } from '@mui/system';
 
-const CustomBottomNavigation = styled(BottomNavigation)({
-  position: 'fixed',
-  bottom: 0,
-  width: '353px',
-  borderTop: '1px solid #cccccc',
-  '& .Mui-selected': {
-    color: '#f06292',
-  },
-  '& .MuiBottomNavigationAction-root': {
-    color: 'gray',
-  },
-  '& .MuiBottomNavigationAction-root .MuiSvgIcon-root': {
-    color: 'gray',
-  },
-  '& .Mui-selected .MuiSvgIcon-root': {
-    color: '#f06292',
-  }
+const StyledBottomNavigation = styled(BottomNavigation)({
+  width: '100%',
+  height: '60px',
 });
 
-export default function SimpleBottomNavigation() {
+const StyledBottomNavigationAction = styled(BottomNavigationAction)(({ theme }) => ({
+  '&.selected': {
+    color: '#ff69b4',
+    '& .MuiBottomNavigationAction-label': {
+      fontWeight: 'bold',
+    },
+    '& .MuiSvgIcon-root': {
+      color: '#ff69b4',
+    },
+  },
+  '&.notSelected': {
+    color: '#cccccc',
+    '& .MuiBottomNavigationAction-label': {
+      fontWeight: 'normal',
+    },
+    '& .MuiSvgIcon-root': {
+      color: '#cccccc',
+    },
+  },
+  '& .MuiBottomNavigationAction-label': {
+    fontSize: '10px',
+    paddingTop: '2px',
+  },
+  '& .MuiSvgIcon-root': {
+    fontSize: '24px',
+  },
+}));
+
+const SimpleBottomNavigation = () => {
   const [value, setValue] = React.useState(0);
 
   return (
-    <CustomBottomNavigation
-      showLabels
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-    >
-      <BottomNavigationAction label="강의평" icon={<StarBorderIcon />} />
-      <BottomNavigationAction label="시험 정보" icon={<PlagiarismOutlinedIcon />} />
-    </CustomBottomNavigation>
+    <div style={{
+      position: "fixed",
+      bottom: "0",
+      width: '353px',
+      borderTop: '1px solid #eeeeee',
+    }}>
+      <StyledBottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <StyledBottomNavigationAction label="강의평" icon={<StarBorderIcon />}
+        className={value === 0 ? 'selected' : 'notSelected'}/>
+        <StyledBottomNavigationAction label="시험 정보" icon={<PlagiarismOutlinedIcon />}
+        className={value === 1 ? 'selected' : 'notSelected'}/>
+      </StyledBottomNavigation>
+    </div>
   );
 }
+
+export default SimpleBottomNavigation;
