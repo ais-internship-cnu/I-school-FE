0ca3c145eaab5f3dc18f0ee128b0166c14311da8
unknown <dnjfqhd12345@gmail.com>
2024-07-31 14:57:20 +0900

    fix: bottom page에서 강의평이 특정한 강의로만 들어가지는 현상 해결

--- src/components/page/TimeTable/BottomSheet/BottomSheetCourseBlock/index.tsx
+++ src/components/page/TimeTable/BottomSheet/BottomSheetCourseBlock/index.tsx
@@ -8,10 +8,8 @@ const BottomSheetCourseBlock = () => {
   const router = useRouter();
   const { fetchAllBottomSheetCourses, bottomSheetCourseList } = useBottomSheetCourseList();
 
-  const handleReviewClick = () => {
-    bottomSheetCourseList.map(c => {
-      router.push(`/review-page/${encodeURIComponent(c.courseName)}/${encodeURIComponent(c.professor)}/${encodeURIComponent(c.courseId)}`);
-    });
+  const handleReviewClick = (courseName: string, professor: string, courseId: number) => {
+    router.push(`/review-page/${encodeURIComponent(courseName)}/${encodeURIComponent(professor)}/${encodeURIComponent(courseId)}`);
   };
 
   useEffect(() => {
@@ -21,27 +19,29 @@ const BottomSheetCourseBlock = () => {
   }, [fetchAllBottomSheetCourses, bottomSheetCourseList.length]);
 
   return (
-    bottomSheetCourseList.map(c => (
-      <div className="course-block" key={c.courseId}>
-        <h3>{c.courseName}</h3>
-        <p>{c.professor} 교수님</p>
-        <p>{c.major}</p>
-        <p>{c.grade}학년</p>
-        <div className="rating-container">
-          <Rating
-            name="course-rating"
-            value={c.rating}
-            precision={0.5}
-            readOnly
-            sx={{ fontSize: '1rem' }}
-          />
-          <div className="action-buttons">
-            <Button className="action-button" onClick={handleReviewClick}>강의평</Button>
-            <Button className="course-button">추가하기</Button>
+    <>
+      {bottomSheetCourseList.map(c => (
+        <div className="course-block" key={c.courseId}>
+          <h3>{c.courseName}</h3>
+          <p>{c.professor} 교수님</p>
+          <p>{c.major}</p>
+          <p>{c.grade}학년</p>
+          <div className="rating-container">
+            <Rating
+              name="course-rating"
+              value={c.rating}
+              precision={0.5}
+              readOnly
+              sx={{ fontSize: '1rem' }}
+            />
+            <div className="action-buttons">
+              <Button className="action-button" onClick={() => handleReviewClick(c.courseName, c.professor, c.courseId)}>강의평</Button>
+              <Button className="course-button">추가하기</Button>
+            </div>
           </div>
         </div>
-      </div>
-    ))
+      ))}
+    </>
   );
 };
 
