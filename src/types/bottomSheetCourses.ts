376a3feefa1d4be9a5c7060d1f59325751490032
jiiiwonyy <ji__Won_22@naver.com>
2024-07-31 14:26:31 +0900

    first commit

--- src/types/bottomSheetCourses.ts
+++ src/types/bottomSheetCourses.ts
@@ -1,11 +1,24 @@
+// export type BottomSheetCourse = {
+//     courseId: string;
+//     courseName: string;
+//     professor: string;
+//     major: string;
+//     grade: number;
+//     rating: number;
+// };
 export type BottomSheetCourse = {
-    courseId: string;
+    courseId: number;
+    courseCode: string;
     courseName: string;
+    courseRoom: string;
+    courseDay: string;
+    courseStartTime: string;
+    courseEndTime: string;
     professor: string;
-    major: string;
-    grade: number;
     rating: number;
-};
+    grade: number;
+    major: string;
+  };
 
 export type BottomSheetCourses = {
     bottomSheetCourseList: BottomSheetCourse[];
